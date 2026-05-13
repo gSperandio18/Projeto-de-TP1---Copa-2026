7fd5415b0fe29fb292d9be9bb1ef6268b659ac90
leo <aokisachileo@gmail.com>
2026-05-12 22:56:38 -0300

    Arrumar o caminho do package de cada um

--- src/main/java/guiEtelas/loginEsignupFolder/TelaLogin.java
+++ src/main/java/guiEtelas/loginEsignupFolder/TelaLogin.java
@@ -2,7 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package guiEtelas;
+package guiEtelas.loginEsignupFolder;
+
+import guiEtelas.telaLogada;
 
 /**
  *
